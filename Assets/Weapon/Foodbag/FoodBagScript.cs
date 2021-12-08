f725632ce96bb449c397c90701d45268a032f681
NKLam <khoalamvn@gmail.com>
2021-12-08 21:36:53 +0700

    Added food bag pickup manually
    If player can reach food bag, they will pick it up, so they dont have to wait 10s

--- Assets/Weapon/Foodbag/FoodBagScript.cs
+++ Assets/Weapon/Foodbag/FoodBagScript.cs
@@ -15,6 +15,8 @@ public class FoodBagScript : MonoBehaviour
 
     public float returnTime = 10f;
     private float timer = 0f;
+    // This prevent foodbag get picked back right after shoot it
+    private float timeBeforeAllowedRepickup = 1f;
 
     private Rigidbody rb;
 
@@ -26,12 +28,23 @@ public class FoodBagScript : MonoBehaviour
 
     private void Update()
     {
-        // Foog bad return to player if not reached the deliverySpot
+        // Food bag return to player if not reached the deliverySpot
         // within time. For lore reason just say it has super GPS tracking stuff.
         timer += Time.deltaTime;
         if (timer > returnTime)
         {
             LevelManager.instance.player.currentFoodBag += 1;
+            Destroy(this.gameObject);
+        }
+    }
+
+    // If player managed to touch foodbag, add it back immediately
+    private void OnTriggerEnter(Collider other)
+    {
+        FPSController player = other.transform.GetComponent<FPSController>();
+        if (player && timer > timeBeforeAllowedRepickup)
+        {
+            player.currentFoodBag += 1;
             Destroy(this.gameObject);
         }
     }
